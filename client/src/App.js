4eb56072611607f982f3198fcac57b6a94d214d8
Quoc Thinh <qnguyen@noota.io>
2023-12-08 03:53:48 +0100

    FIX enable prod

--- client/src/App.js
+++ client/src/App.js
@@ -16,11 +16,14 @@ const environment = process.env.NODE_ENV
  * Set enableMock(Default false) to true at configs/app.config.js
  * If you wish to enable mock api
  */
-if (environment !== 'production' && appConfig.enableMock) {
-    mockServer({ environment })
-}
+// if (environment !== 'production' && appConfig.enableMock) {
+//     mockServer({ environment })
+// }
+
+
 
 function App() {
+
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
